3242dc54b779fac7433b2a45cabf3a0b65728cf6
ethanlockwood <lockwood.ethan@yahoo.com>
2025-05-25 16:03:12 -0400

    Figured out how to use the routeroutlet to display components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
