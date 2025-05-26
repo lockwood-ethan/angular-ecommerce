c1fa3e3717a1bfee46618383c98d94065811091f
ethanlockwood <lockwood.ethan@yahoo.com>
2025-05-26 07:21:17 -0400

    Created http client and for interacting with the backend

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'localhost:8080/api/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.http.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
