8ed5bfb5830049bb59d4dacb139c7a6cd8f44673
ethanlockwood <lockwood.ethan@yahoo.com>
2025-05-26 09:59:30 -0400

    Set up products-list component to make api call using the http client, display results on the web page

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
+import { NgFor } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [NgFor, CurrencyPipe],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
 
+  products: Product[] = [];
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import { map } from 'rxjs';
 })
 export class ProductService {
 
-  private baseUrl = 'localhost:8080/api/products';
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private http: HttpClient) { }
 
